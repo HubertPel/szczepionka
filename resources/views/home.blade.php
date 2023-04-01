2435dd80da050a17d1a82614fc45dec69dedf36c
WojtekPcj <wojtekpaciej@gmail.com>
2023-04-01 13:12:52 +0200

    Background i logowanie

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,12 @@
 @include('templates.header')
-
+<html class="background">
 <div class="header_main">
     <div>testing</div>
     <div>testing</div>
     <div>testing</div>
 </div>
-
 <h1 class="test-style">Witaj w Szczepionkowie</h1>
 <div>Zapraszamy na szczepienia dostępne w naszej ofercie.</div>
-<p><a href="/logowanie">Zaloguj sie</a></p>
-
+<p class="login"><a href="/logowanie">Zaloguj sie</a></p>
+</html>
 @include('templates.footer')
--- resources/views/templates/header.blade.php
+++ resources/views/templates/header.blade.php
@@ -4,6 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="{{ asset('css/header.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/background.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
     <title>Szczepionka</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <script src="{{asset('js/app.js')}}"></script>
