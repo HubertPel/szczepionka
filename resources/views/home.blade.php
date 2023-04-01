aee1fe461dd109d99448602d609ebdb1a7669589
Sebastian Piechowski <duneval@gmail.com>
2023-04-01 03:04:32 +0200

    test content in home.blade.php file

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,13 @@
 @include('templates.header')
 
-<h1 class="test-style"> Strona glowna </h1>
+<div class="header_main">
+    <div>testing</div>
+    <div>testing</div>
+    <div>testing</div>
+</div>
+
+<h1 class="test-style">Witaj w Szczepionkowie</h1>
+<div>Zapraszamy na szczepienia dostępne w naszej ofercie.</div>
 <p><a href="/logowanie">Zaloguj sie</a></p>
 
 @include('templates.footer')
