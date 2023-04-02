1a18393ca241e729874e25caa1c4eab44d3a9d07
Sebastian Piechowski <duneval@gmail.com>
2023-04-03 01:56:40 +0200

    navbar - 60%

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,5 @@
 @include('templates.header')
-
-<div class="header_main">
-    <div>testing</div>
-    <div>testing</div>
-    <div>testing</div>
-</div>
+@include('templates.navbar')
 
 <div class="main-image">
     <div class="main-image-text">
@@ -18,17 +13,28 @@
     </div>
 </div>
 
-<div class="main-container">
+<div class="main-container body">
     <div class="content">
         <p class='title'>O nas</p>
         <hr>
         <p>
-            Rejestracja na szczepienia przeciw COVID-19 online to proces, który umożliwia osobom zarejestrowanie się na szczepienie poprzez platformę internetową. W wielu krajach rejestracja online stała się standardowym sposobem rezerwacji terminu na szczepienie, co umożliwia szybsze i bardziej wygodne zarządzanie procesem szczepień.
+            Rejestracja na szczepienia przeciw COVID-19 online to proces, który umożliwia osobom zarejestrowanie się na
+            szczepienie poprzez platformę internetową. W wielu krajach rejestracja online stała się standardowym
+            sposobem rezerwacji terminu na szczepienie, co umożliwia szybsze i bardziej wygodne zarządzanie procesem
+            szczepień.
         </p>
-        <p>Osoby zainteresowane szczepieniem mogą odwiedzić stronę internetową, na której są dostępne informacje na temat szczepionek przeciw COVID-19, procedury szczepień oraz lokalizacje, w których szczepienia są wykonywane. W większości przypadków wymagane jest podanie informacji osobistych, takich jak imię, nazwisko, numer telefonu i adres e-mail, aby zarejestrować się na szczepienie.</p>
-        <p>Po zarejestrowaniu, osoba może zazwyczaj wybrać dogodny dla siebie termin szczepienia i lokalizację, w której chce się zaszczepić. Po dokonaniu rezerwacji, użytkownik otrzymuje potwierdzenie e-mailem lub SMS-em, a niektóre platformy mogą również przypominać o terminie szczepienia w dniu jego wykonania.</p>
-        <p>Zapisy na szczepienia online umożliwiają łatwy i szybki dostęp do szczepień przeciw COVID-19, co przyczynia się do przyspieszenia procesu szczepień i zwiększenia liczby osób zaszczepionych. Platformy te mogą również zawierać informacje na temat bezpieczeństwa i skuteczności szczepionek, co może pomóc w zmniejszeniu obaw i wątpliwości związanych z procesem szczepień.</p>
-      </div>
+        <p>Osoby zainteresowane szczepieniem mogą odwiedzić stronę internetową, na której są dostępne informacje na
+            temat szczepionek przeciw COVID-19, procedury szczepień oraz lokalizacje, w których szczepienia są
+            wykonywane. W większości przypadków wymagane jest podanie informacji osobistych, takich jak imię, nazwisko,
+            numer telefonu i adres e-mail, aby zarejestrować się na szczepienie.</p>
+        <p>Po zarejestrowaniu, osoba może zazwyczaj wybrać dogodny dla siebie termin szczepienia i lokalizację, w której
+            chce się zaszczepić. Po dokonaniu rezerwacji, użytkownik otrzymuje potwierdzenie e-mailem lub SMS-em, a
+            niektóre platformy mogą również przypominać o terminie szczepienia w dniu jego wykonania.</p>
+        <p>Zapisy na szczepienia online umożliwiają łatwy i szybki dostęp do szczepień przeciw COVID-19, co przyczynia
+            się do przyspieszenia procesu szczepień i zwiększenia liczby osób zaszczepionych. Platformy te mogą również
+            zawierać informacje na temat bezpieczeństwa i skuteczności szczepionek, co może pomóc w zmniejszeniu obaw i
+            wątpliwości związanych z procesem szczepień.</p>
+    </div>
 </div>
 
 @include('templates.footer')
---
+++ resources/views/templates/navbar.blade.php
@@ -0,0 +1,24 @@
+<link rel="stylesheet" href="{{ asset('css/nav_bar.css') }}">
+
+<header class="site-navbar site-navbar-target" role="banner">
+    <div class="container">
+        <div class="row align-items-center position-relative">
+            <div class="col-3">
+                <div class="site-logo">
+                    <img src="public/images/icons/health-injenction-medical-svgrepo-com.svg" alt="Szczepionkowo">
+                </div>
+            </div>
+            <div class="col-9 text-right">
+                <nav class="site-navigation text-right ml-auto d-none d-lg-block" role="navigation">
+                    <ul class="site-menu main-menu js-clone-nav ml-auto">
+                        <li class="active">
+                            <a href="#" class="nav-link">Home</a>
+                        </li>
+                    </ul>
+                </nav>
+
+            </div>
+        </div>
+    </div>
+
+</header>
